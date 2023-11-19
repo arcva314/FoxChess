c8b942dbbf5ea9114af8dd8084264d432a3d13ea
arcva314 <147210506+arcva314@users.noreply.github.com>
2023-11-18 19:38:56 -0800

    Add files via upload

--- FoxGame.py
+++ FoxGame.py
@@ -24,6 +24,8 @@ class Game():
             self.white_king_location = (move.end_row, move.end_col)
         elif move.piece == 'bK':
             self.black_king_location = (move.end_row, move.end_col)
+        if move.is_pawn_promotion:
+            self.board[move.end_row][move.end_col] = move.piece[0] + 'Q'
     def get_valid_moves(self):
         moves = []
         self.inCheck, self.pins, self.checks = self.find_pins_and_checks()
@@ -130,7 +132,7 @@ class Game():
         for r in range(len(self.board)):
             for c in range(len(self.board[r])):
                 turn = self.board[r][c][0]
-                if (turn == 'w' and self.whiteToMove) or (turn == 'b' and not self.whiteToMove):
+                if ((turn == 'w' or turn == 'f') and self.whiteToMove) or ((turn == 'b' or turn == 'f') and not self.whiteToMove):
                     piece = self.board[r][c][1]
                     if piece == 'p':
                         self.get_pawn_moves(r, c, moves)
@@ -144,6 +146,9 @@ class Game():
                         self.get_Queen_Moves(r, c, moves)
                     elif piece == 'K':
                         self.get_King_Moves(r, c, moves)
+                    elif piece == 'f':
+                        self.get_fox_moves(r, c, moves)
+
         return moves
 
 
@@ -258,7 +263,7 @@ class Game():
             if 0 <= endRow < 8 and 0 <= endCol < 8:
                 if not piecePinned:
                     endPiece = self.board[endRow][endCol]
-                    if endPiece[0] != ally_Color:
+                    if endPiece[0] != ally_Color and endPiece != 'ff':
                         moves.append(Move((r, c), (endRow, endCol), self.board))
 
     def get_Queen_Moves(self, r, c, moves):
@@ -274,18 +279,26 @@ class Game():
             endCol = c + colMoves[i]
             if 0 <= endRow < 8 and 0 <= endCol < 8:
                 endPiece = self.board[endRow][endCol]
-                if endPiece[0] != allyColor:
-                    if allyColor == "w":
-                        self.white_king_location = (endRow, endCol)
-                    else:
-                        self.black_king_location = (endRow, endCol)
-                    inCheck, pins, checks = self.find_pins_and_checks()
-                    if not inCheck:
-                        moves.append(Move((r, c), (endRow, endCol), self.board))
-                    if allyColor == "w":
-                        self.white_king_location = (r, c)
-                    else:
-                        self.black_king_location = (r, c)
+                if endPiece != 'ff':
+                    if endPiece[0] != allyColor:
+                        if allyColor == "w":
+                            self.white_king_location = (endRow, endCol)
+                        else:
+                            self.black_king_location = (endRow, endCol)
+                        inCheck, pins, checks = self.find_pins_and_checks()
+                        if not inCheck:
+                            moves.append(Move((r, c), (endRow, endCol), self.board))
+                        if allyColor == "w":
+                            self.white_king_location = (r, c)
+                        else:
+                            self.black_king_location = (r, c)
+    def get_fox_moves(self, r, c, moves):
+        for i in range(8):
+            for j in range(8):
+                if self.board[i][j] == '--':
+                    moves.append(Move((r,c), (i,j), self.board))
+        #print("hello")
+
 class Move:
     def __init__(self, start_square, end_square, board):
         self.start_row = start_square[0]
@@ -293,6 +306,8 @@ class Move:
         self.end_row = end_square[0]
         self.end_col = end_square[1]
         self.piece = board[self.start_row][self.start_col]
+        self.is_pawn_promotion = (self.piece == 'wp' and self.end_row == 0) or (
+        self.piece == 'bp' and self.end_row == 7)
         self.moveID = self.start_row * 1000 + self.start_col * 100 + self.end_row * 10 + self.end_col
     def __eq__(self, other):
         if isinstance(other, Move):
